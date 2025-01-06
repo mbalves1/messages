ccbe3c3ac9631bc8e312d6959dd73be3e13a6bba
Murilo <mbalves1@outlook.com>
2025-01-06 10:12:54 -0300

    `[refactor]: Refactor MessagesController and MessagesService to use constructor injection`

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -1,9 +1,10 @@
-import { promises as fs } from 'fs';
+// import { promises as fs } from 'fs';
+import { readFile, writeFile } from 'fs/promises';
 
 export class MessagesRepository {
   async findOne(id: string): Promise<string | undefined> {
     try {
-      const contents = await fs.readFile('messages.json', 'utf-8'); // Use 'utf-8' corretamente
+      const contents = await readFile('messages.json', 'utf-8'); // Use 'utf-8' corretamente
       const messages = JSON.parse(contents);
       return messages[id];
     } catch (error) {
@@ -14,7 +15,7 @@ export class MessagesRepository {
 
   async findAll() {
     try {
-      const contents = await fs.readFile('messages.json', 'utf-8');
+      const contents = await readFile('messages.json', 'utf-8');
       const messages = JSON.parse(contents);
       return messages;
     } catch (error) {
@@ -25,13 +26,13 @@ export class MessagesRepository {
 
   async create(content: string) {
     try {
-      const contents = await fs.readFile('messages.json', 'utf-8');
+      const contents = await readFile('messages.json', 'utf-8');
       const messages = JSON.parse(contents);
 
       const id = Math.floor(Math.random() * 999);
       messages[id] = { id, content };
 
-      await fs.writeFile('messages.json', JSON.stringify(messages));
+      await writeFile('messages.json', JSON.stringify(messages));
     } catch (error) {
       console.error('Error creating a message:', error);
     }
